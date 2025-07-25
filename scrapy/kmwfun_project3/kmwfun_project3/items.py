873c9d129899c856dcf4404e9571abf61234110a
kmw101989 <kmw101989@gmail.com>
2025-07-25 17:32:42 +0900

    250725_v3

--- scrapy/kmwfun_project3/kmwfun_project3/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class KmwfunProject3Item(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    category = scrapy.Field()
-    
-    pass 
